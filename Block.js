1b4fd70574c04b8b21f5d323faf4697ddf8f0cea
vinayannam <vinay1011979.7@gmail.com>
2018-04-15 22:29:22 +0530

    .

--- Block.js
+++ Block.js
@@ -13,7 +13,7 @@ class Block {
             0,
             "0",
             0,
-            "Welcome to BMU Blockchain",
+            "{message:Welcome to BMU Blockchain}",
             "000dc75a315c77a1f9c98fb6247d03dd18ac52632d7dc6a9920261d8109b37cf",
             0
         );
